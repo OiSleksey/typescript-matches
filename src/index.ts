4f79577079e9b9d57a04e042f6707985593d0fa8
OiSleksey <116756218+OiSleksey@users.noreply.github.com>
2023-07-07 14:21:31 +0300

    debugging the project

---
+++ src/index.ts
@@ -0,0 +1,38 @@
+import fs from 'fs';
+
+const matches = fs
+  .readFileSync('football.csv', { encoding: 'utf-8' })
+  .split('\n')
+  .map((row: string): string[] => {
+    return row.split(',');
+  });
+
+// const arrMatches = matches.split('\n');
+console.log(matches[0]);
+
+enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D',
+}
+
+// const getMatchResult = (match: string[]): MatchResult => {
+//   if (match[5] === 'H') {
+//     return MatchResult.HomeWin;
+//   } else if (match[5] === 'A') {
+//     return MatchResult.AwayWin;
+//   }
+//   return MatchResult.Draw;
+// };
+
+let manUnitedWins = 0;
+
+for (let match of matches) {
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+    manUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Man United won ${manUnitedWins} times`);
